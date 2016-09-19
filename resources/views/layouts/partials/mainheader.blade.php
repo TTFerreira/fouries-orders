8998bd947d64d420841d77cd2f2d41c11ae82a6e
Terry Ferreira <terry@pixelcandy.co.za>
2016-09-19 12:57:52 +0200

    Fixed menu to be responsive

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -2,17 +2,24 @@
 <header class="main-header">
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
-      <!-- Logo -->
-      <a href="{{ url('/') }}" class="navbar-brand">
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Fouries</b>Orders </span>
-      </a>
-      <ul class="nav navbar-nav">
-          <li><a href="{{ url('orders') }}"><i class='fa fa-shopping-cart'></i> <span>Orders</span></a></li>
-          @role(['super-admin', 'admin'])
-            <li><a href="{{ url('/admin')}}"><i class='fa fa-gear'></i> <span>Administrator</span></a></li>
-          @endrole
-      </ul>
+      <div class="navbar-header">
+        <!-- Logo -->
+        <a href="{{ url('/') }}" class="navbar-brand">
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Fouries</b>Orders </span>
+        </a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li><a href="{{ url('orders') }}"><i class='fa fa-shopping-cart'></i> <span>Orders</span></a></li>
+            @role(['super-admin', 'admin'])
+              <li><a href="{{ url('/admin')}}"><i class='fa fa-gear'></i> <span>Administrator</span></a></li>
+            @endrole
+        </ul>
+      </div>
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
                 @if (Auth::guest())
